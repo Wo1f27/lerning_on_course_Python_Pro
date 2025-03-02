bf424aee2595d08cd6d5005568c8966196adb95f
Wo1f27 <avstrelkov@yandex.ru>
2025-03-02 13:09:07 +0300

    Upd homework_5

--- homework_5.py
+++ homework_5.py
@@ -1,18 +1,21 @@
 import time
 class Task:
 
-    def __init__(self, name: str, duration: str):
+    def __init__(self, name: str, duration: str, priority: str):
         self.name = name
         self.duration = duration
+        self.priority = priority
+        self.status = 'Pending'
 
     def __repr__(self):
-        return f'Task: {self.name}, Duration: {self.duration}'
+        return f'Task: {self.name}, Duration: {self.duration}, Priority: {self.priority}, Status: {self.status}'
 
 
 class TaskScheduler:
 
     def __init__(self):
         self.queue = Queue()
+        self.res_arr = []
 
     def add_task(self, task: Task) -> str:
         try:
@@ -25,15 +28,28 @@ class TaskScheduler:
         while not self.queue.is_empty():
             start_time = time.time()
             task = self.queue.dequeue()
+            task.status = 'In progress'
             time.sleep(float(task.duration))
+            task.status = 'Completed'
+            self.res_arr.append(task)
             end_time = time.time()
             print(f'Задача {task.name} выполнена, время выполнения: {end_time - start_time}')
+
+    def interrupt_current_task(self):
+        if self.queue:
+            current_task = self.queue.dequeue()
+            current_task.status = "Interrupted"
+            print(f"Прервано выполнение {current_task.name}. Переход к следующей задаче.")
+            self.queue.dequeue()
 
     def is_empty(self) -> bool:
         return self.queue.is_empty()
 
     def count_tasks(self) -> int:
         return len(self.queue)
+
+    def get_task_statuses(self, arr):
+        return [(task.name, task.status) for task in arr]
 
 
 class Queue:
@@ -58,18 +74,20 @@ def main():
     tasks_scheduler = TaskScheduler()
 
     tasks = [
-        Task('Task#1', '2'),
-        Task('Task#2', '2.5'),
-        Task('Task#3', '1.5'),
-        Task('Task#4', '2'),
-        Task('Task#5', '2.5')
+        Task('Task#1', '2', '1'),
+        Task('Task#2', '2.5', '2'),
+        Task('Task#3', '1.5', '1'),
+        Task('Task#4', '2', '4'),
+        Task('Task#5', '2.5', '5')
     ]
 
     for i in tasks:
         print(tasks_scheduler.add_task(i))
 
     print(f'Количество задач в очереди: {tasks_scheduler.count_tasks()}')
+    print(f'Статусы задач: {tasks_scheduler.get_task_statuses(tasks_scheduler.queue.items)}')
     tasks_scheduler.execute_tasks()
+    print(f'Статусы задач: {tasks_scheduler.get_task_statuses(tasks_scheduler.res_arr)}')
 
 
 if __name__ == '__main__':
